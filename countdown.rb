485d7f5e8a82731a957b63051b589eaff90aafa6
Bill G <gbilly3@gmail.com>
2019-02-10 17:10:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -18,20 +18,6 @@ end
 end
 
 
-
-def countdown_with_sleep(number)
-  
- def countdown(number)
-  number = 10
-while number > 0
-number -= 1 && sleep 1
-  puts "#{number} SECOND(S)!"
-  
-end
-end 
- puts  "HAPPY NEW YEAR!"
-
-  
   
 end 
 end 
